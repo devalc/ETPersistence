0eecd1ab35375a2874c76fc487f2fa343cac8f86
devalc <deva4998@vandals.uidaho.edu>
2022-02-28 17:29:22 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 window.onload = intiMap;
 
-function intiMap(){
-var map = new ol.Map({
+function intiMap() {
+
+  var map = new ol.Map({
     target: 'map',
     layers: [
       new ol.layer.Tile({
-        source: new ol.source.OSM({layer: 'terrain'})
+        source: new ol.source.OSM({ layer: 'terrain' })
       })
     ],
     view: new ol.View({
       center: ol.proj.fromLonLat([-114.0, 42.5]),
-      zoom: 10
+      zoom: 8
     })
   })
+
+  // vector layer- Water Districts
+  const dist = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      format: new ol.format.GeoJSON(),
+      url: './data/Water_Districts.geojson'
+    })
+  })
+
+  // Raster layer
+
+
+
+  // add everything to base map layer
+
+  map.addLayer(dist)
+
 }
+
